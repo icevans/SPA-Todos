59b5489570b7c631e15322a36c926c15d9ab9f9f
Ian Evans <icevans@icloud.com>
2019-08-07 12:28:49 -0600

    Add TodoList class

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,6 +17,15 @@ class Todo {
     this.completed = true;
   }
 
+  update(props) {
+    this.title = props.title;
+    this.day = props.day;
+    this.month = props.month;
+    this.year = props.year;
+    this.completed = props.completed;
+    this.description = props.description;
+  }
+
   groupString() {
     if (!this.month || !this.year) {
       return null;
@@ -25,3 +34,55 @@ class Todo {
     return this.month + '/' + this.year;
   }
 }
+
+class TodoList {
+  constructor(propsList) {
+    this.todos = this.propsToTodos(propsList)
+  }
+
+  propsToTodos(propsList) {
+    return propsList.map(props => new Todo(props));
+  }
+
+  add(props) {
+    this.todos.push(new Todo(props));
+  }
+
+  fetchTodo(id) {
+    return this.todos.filter(todo => todo.id === id)[0];
+  }
+
+  complete(id) {
+    this.fetchTodo(id).complete();
+  }
+
+  toggle(id) {
+    this.fetchTodo(id).toggle();
+  }
+
+  update(id, props) {
+    this.fetchTodo(id).update(props);
+  }
+
+  remove(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  filter(groupString) {
+    return this.todos.filter(todo => todo.groupString() === groupString);
+  }
+}
+
+const props = { id: 1, title: 'todo item', day: '01', month: '04', year: '19',
+  completed: false, description: '',
+};
+
+const props2 = { id: 2, title: 'todo item', day: '01', month: '04', year: '19',
+  completed: false, description: '',
+};
+
+const props3 = { id: 3, title: 'todo item', day: '01', month: '05', year: '19',
+  completed: false, description: '',
+};
+
+const todos = new TodoList([props]);
